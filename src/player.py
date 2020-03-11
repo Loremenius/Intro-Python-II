2b6053140750359e43cd48deee8631f4ed1395f6
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-11 16:56:10 -0600

    completed day 1 tasks

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player():
+    def __init__(self, room):
+        self.current_room = room
+    
+    def move_room(self):
+        self.current_room.display_info()
+        move = input("\nWhich way will you go: ").lower().strip()
+        if move == "q":
+            exit()
+        else:
+            move_check = self.current_room.check_move(move)
+        
+        if move_check[0] == False:
+            print(f"\n{move_check[1]}")
+            self.move_room()
+        else:
+            self.current_room = move_check[1]
+            self.move_room()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,38 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+    
+    def display_info(self):
+        print(f"\nYou are in {self.name}. {self.description}")
+# return { boolean:false, message: "Must be direction north, south, east, or west"}
+    def check_move(self, move):
+        if move == "north":
+            if self.n_to == None:
+                return False, "There is nothing to the north"
+            else:
+                return True, self.n_to
+        elif move == "south":
+            if self.s_to == None:
+                return False, "There is nothing to the south"
+            else:
+                return True, self.s_to
+        elif move == "west":
+            if self.w_to == None:
+                return False, "There is nothing to the west"
+            else:
+                return True, self.w_to
+        elif move == "east":
+            if self.e_to == None:
+                return False, "There is nothing to the east"
+            else:
+                return True, self.e_to
+        else:
+            return False, "Must be direction north, south, east, or west"  
+        
